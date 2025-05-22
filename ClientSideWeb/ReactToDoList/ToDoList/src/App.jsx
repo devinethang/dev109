d0acff1c5eb9507a30a40a8dfc710e05d8a77f1c
Devin Graupmann <devinethang@gmail.com>
2025-05-21 23:54:38 -0700

    css errors

--- ClientSideWeb/ReactToDoList/ToDoList/src/App.jsx
+++ ClientSideWeb/ReactToDoList/ToDoList/src/App.jsx
@@ -1,23 +1,21 @@
+import './main.css'
 import { useState } from 'react'
 import './App.css'
-import RegistrationForm from './RegistrationForm.jsx'
-import LoginForm from './LoginForm.jsx'
 import AuthSection from './AuthSection.jsx'
 import UserSection from './UserSection.jsx'
 import EditModal from './EditModal.jsx'
-import todo from './todo.js'
-
-// Example: use todo in your React app
-console.log('Todo module:', todo)
+import './todo.js'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <AuthSection></AuthSection>
-      <UserSection></UserSection>
-      <EditModal></EditModal>
+      <div>
+        <AuthSection></AuthSection>
+        <UserSection></UserSection>
+        <EditModal></EditModal>
+      </div>
     </>
   )
 }
