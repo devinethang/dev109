441bff85ad4b299d805eac187c5906fbee99cca0
Devin Graupmann <devinethang@gmail.com>
2025-06-07 21:09:22 -0700

    Finished m09

---
+++ m09/js/add-element.js
@@ -0,0 +1,41 @@
+// Description: This script adds a new element to an existing list in the HTML document.
+
+function addElement() {
+    // Create a new element and store it in a variable.
+    var newEl = document.createElement('li');
+
+    // Create a text node and store it in a variable.
+    var newText = document.createTextNode(document.getElementById('new-item').value);
+
+    // Send error if the input is empty.
+    let error = null;
+    try {
+        if (newText.textContent.trim() === '') {
+            error = "The entry was empty";
+            throw new Error('Input cannot be empty');
+        }
+        // Attach the new text node to the new element.
+        newEl.appendChild(newText);
+
+        // Find the position where the new element should be added.
+        var position = document.getElementsByTagName('ul')[0];
+
+        // Insert the new element into its position.
+        position.appendChild(newEl);
+    }
+
+    catch (e) {
+        console.log(e);
+    }
+    finally {
+        // I did E.C.
+        // you did E.C.
+        if (error) {
+            console.log(error);
+        }
+    }
+
+
+}
+
+
