33c8873f89ed4356081a1f43cdee8b4b117cc21e
Devin Graupmann <devinethang@gmail.com>
2025-06-25 00:17:01 -0700

    Changed image links to local files and added changed alt text on next image

--- a5/js/main.js
+++ a5/js/main.js
@@ -1,21 +1,31 @@
 let currentIndex = 0;
+
 let currentimage = document.getElementById("carousel-image");
 
 let images = [
-    "https://images.unsplash.com/photo-1506744038136-46273834b3fb",
-    "https://images.pexels.com/photos/34950/pexels-photo.jpg",
-    "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
-    "https://images.unsplash.com/photo-1465101046530-73398c7f28ca",
-    "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg"
+    "./images/image1.jpg",
+    "./images/image2.jpg",
+    "./images/image3.jpg",
+    "./images/image4.jpg",
+    "./images/image5.jpeg"
 ];
+
+let altTexts = [
+    "A beautiful sunset in between two mountains",
+    "A railroad track leading into the distance inside a forest",
+    "A tree on an island with the sun shining through the center",
+    "A beutiful view of the stars in the night sky",
+    "A dock leading out to a lake with mountains in the background"
+];
+
 function nextImage() {
     currentIndex = (currentIndex + 1) % images.length;
     currentimage.src = images[currentIndex];
-    return images[currentIndex];
+    currentimage.alt = altTexts[currentIndex];
 }
 
 function previousImage() {
     currentIndex = (currentIndex - 1 + images.length) % images.length;
     currentimage.src = images[currentIndex];
-    return images[currentIndex];
+    currentimage.alt = altTexts[currentIndex];
 }
